6da17ccdcff74aa7c02e021a8a17c99445bd0779
Sitansu Mandal <85841972+sitansu04@users.noreply.github.com>
2023-01-18 01:45:56 +0530

    login singup backend

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { connection } = require("./config/db.js");
+const { userRouter } = require("./routes/user.routes.js");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use("/users", userRouter);
+
+app.get("/", async (req, res) => {
+  res.send({ msg: "API STARTED" });
+});
+
+app.listen(process.env.port, async (req, res) => {
+  try {
+    await connection;
+    console.log("connected to db");
+    console.log(`Connection Established at ${process.env.port}`);
+  } catch (error) {
+    console.log({ msg: "error in connextion to DB" });
+    console.log(error.message);
+  }
+});
